7f2d1cdf124fbe7ddd35d6ddca1df74ee7a2a41c
Diego Vasco Dias Da Silva <72607505+jackbluecx@users.noreply.github.com>
2025-11-14 01:48:50 -0300

    primeiro upload

---
+++ chatgpt.lua
@@ -0,0 +1,5 @@
+-- ~/.config/devilspie2/chatgpt.lua
+if get_window_class() == "WebApp-ChatGPT4319" then
+    set_window_geometry(1320, 680, 600, 400) -- X, Y, largura, altura
+    make_always_on_top()
+end
