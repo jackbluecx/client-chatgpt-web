db5843ba482444ff74ee5b08b4d0a44c6f3fb76b
Diego Vasco Dias Da Silva <72607505+jackbluecx@users.noreply.github.com>
2025-11-20 00:20:28 -0300

    Delete chatgpt.lua

--- chatgpt.lua
+++
@@ -1,5 +0,0 @@
--- ~/.config/devilspie2/chatgpt.lua
-if get_window_class() == "WebApp-ChatGPT4319" then
-    set_window_geometry(1320, 680, 600, 400) -- X, Y, largura, altura
-    make_always_on_top()
-end
